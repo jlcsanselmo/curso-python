23ab2afa9dcb476a15b98dd48476050b5e11e659
João Lucas <159937337+jlcsanselmo@users.noreply.github.com>
2025-08-31 19:16:14 -0300

    aula 7 - variáveis

---
+++ aula7.py
@@ -0,0 +1,15 @@
+nome = 'Joao'
+sobrenome = 'Silva'
+idade = 25
+ano_atual = 2024
+ano_nascimento = 2025 - idade
+maior_idade = idade >= 18
+altura_metros = 1.75
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros) 
